d7effc41d71cd1e5da6d56a68cff8937ca7c0a4c
Stuti-Gujarathi <88187880+Stuti-Gujarathi@users.noreply.github.com>
2022-08-03 11:33:45 +0530

    Add files via upload

---
+++ ImplicitCasting.java
@@ -0,0 +1,18 @@
+class ImplicitCasting
+{
+public static void main(String args[])
+{
+byte i= 50;
+short j= i;
+int k= j;
+long l=k;
+float m= l;
+double n = m;
+System.out.println("byte value:"+i);
+System.out.println("short value:"+j);
+System.out.println("int value:"+k);
+System.out.println("long value:"+l);
+System.out.println("float value:"+m);
+System.out.println("double value:"+n);
+}
+}
