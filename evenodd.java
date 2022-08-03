0f71ce5a291c4c5305350fdefff4ed8289491fb7
Stuti-Gujarathi <88187880+Stuti-Gujarathi@users.noreply.github.com>
2022-08-03 11:29:50 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,15 @@
+class evenodd
+{
+public static void main(String args[])
+{
+int num=1250;
+if(num%2==0)
+{
+System.out.println("Even number");
+}
+else
+{
+System.out.println("Odd number");
+}
+}
+}
