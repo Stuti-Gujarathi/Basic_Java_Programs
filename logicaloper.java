a91e4cdf8f3061ff6c8dac8238ec42f6feb4676c
Stuti-Gujarathi <88187880+Stuti-Gujarathi@users.noreply.github.com>
2022-08-03 11:28:25 +0530

    Add files via upload

---
+++ logicaloper.java
@@ -0,0 +1,15 @@
+class logicaloper
+{
+public static void main(String args[])
+{
+int num=-10;
+if(num>0||num==0)
+{
+System.out.println("Positive Number");
+}
+else
+{
+System.out.println("Negative Number");
+}
+}
+}
